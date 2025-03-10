47384f043fa5e6e5dd7c5e52a1995a1ec28245b9
MRBOX <olamigokeadebayo90@gmail.com>
2025-03-10 15:24:48 +0100

    Hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
@@ -14,6 +16,10 @@ const Home = () => {
           <img src={hero_title} alt="" className='caption-img' />
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
              Quas architecto, consectetur ipsa cum nesciunt sunt.</p>
+             <div className="hero-btns">
+              <button className='btn'><img src={play_icon} alt="" /> Play</button>
+              <button className='btn dark-btn'><img src={info_icon} alt="" /> More Info</button>
+             </div>
         </div>
       </div>
     </div>
