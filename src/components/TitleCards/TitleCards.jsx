5452c978116ca62a6dce42803c4066eeadbb45d1
MRBOX <olamigokeadebayo90@gmail.com>
2025-03-12 14:22:53 +0100

    Hero Section-cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import card_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+
+
+
+const TitleCards = ({title, category}) => {
+
+
+
+  const cardsRef = useRef();
+
+const handleWheel = (event)=> {
+  event.preventDefault;
+  cardsRef.current.scroolLeft += event.deltaY
+}
+
+useEffect(() => {
+  cardsRef.current.addEventListener('wheel', handleWheel)
+}, [])
+ 
   return (
-    <div className='titlecards'>
-      
+    <div className='title-cards'>
+      <h2>{title?title:"Popular On Netflix"}</h2>
+
+      <div className="card-list" ref={cardsRef}>
+        {card_data.map((card, index) =>{
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
