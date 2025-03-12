6a8010cb4dd45edbd672609e512c1f5b067be376
MRBOX <olamigokeadebayo90@gmail.com>
2025-03-12 14:46:34 +0100

    Adding the Reat Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Home from './pages/Home/Home'
 import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login/Login'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/'/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
       </Routes>
-      <Home />
+      
       
 
     </div>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './login.css'
+import logo from '../../assets/logo.png'
 
 const Login = () => {
   return (
     <div className='login'>
-      
+      <img src={logo} alt="" className='login-logo' />
+      <div className="login-form">
+        <h1>Sign In</h1>
+        <form>
+          <input type="text" placeholder='Your name' />    
+          <input type="email" placeholder='Email' />    
+          <input type="password" placeholder='Password' />  
+          <button>Sign In</button>
+          <div className='form-help'>
+            <div className='remeber'>
+              <input type="checkbox" name="" id="" />
+              <label htmlFor="">Remeber Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>  
+        </form>
+      </div>
     </div>
   )
 }
